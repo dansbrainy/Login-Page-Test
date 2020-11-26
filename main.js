75c98f66f776277cc6d6c2b9f5f5027903c7c8e8
Daniel Kamanda <danskamanda@gmail.com>
2020-11-26 15:52:29 +0200

    Edit Script

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-$(document).ready(function(){
-    $(document).on('click', 'span.login, span.register', function(){
-        $( "#login" ).toggleClass("hide");
-        $(this).toggleClass( "show" );
-    });
- });
+var x = document.getElementById("register")
+var y = document.getElementById("login")
+
+function register() {
+    x.classList.remove("hide")
+    y.classList.add("hide")
+}
+
+function login() {
+    x.classList.add("hide")
+    y.classList.remove("hide")
+}
